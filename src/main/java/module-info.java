8fc215eb8197d35a71c36b7e5f3b2ea3e908837e
Svene <devaidrecabrera@gmail.com>
2022-12-15 12:09:20 +0800

    Upload for John

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module um.galang.cce109.projectgalang {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens um.galang.cce109.projectgalang to javafx.fxml;
     exports um.galang.cce109.projectgalang;
 }
